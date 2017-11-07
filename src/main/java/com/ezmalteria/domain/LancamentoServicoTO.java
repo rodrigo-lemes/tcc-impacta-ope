d42c8813243a7e3a4b0f8732ca7dcf74b6975cc1
rodrigo@proponto-dev <ti.03@proponto.com.br>
2017-11-07 11:43:04 -0200

    some more jdbc X jpa refactoring.. bit tired

--- src/main/java/com/ezmalteria/domain/LancamentoServicoTO.java
+++ src/main/java/com/ezmalteria/domain/LancamentoServicoTO.java
@@ -1,5 +1,6 @@
 package com.ezmalteria.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -9,45 +10,85 @@ import javax.persistence.Table;
 public class LancamentoServicoTO {
 
 	@Id
+	@Column(name="idlancamento")
 	private String idLancamento;
+	@Column(name="nomefuncionario")
 	private String nomeFuncionario;
+	@Column(name="codfunc")
 	private String codFunc;
+	@Column(name="nomecliente")
 	private String nomeCliente;
+	@Column(name="comanda")
 	private String comanda;
+	@Column(name="alicate")
 	private String alicate;
+	@Column(name="idtiposervico")
 	private String idTipoServico;
+	@Column(name="valor")
 	private String valor;
+	@Column(name="tipofuncionario")
 	private String tipoFuncionario;
+	@Column(name="datainclusao")
 	private String dataInclusao;
+	@Column(name="dataalteracao")
 	private String dataAlteracao;
+	@Column(name="databaixa")
 	private String dataBaixa;
+	@Column(name="manicurepreco")
 	private String manicurePreco;
+	@Column(name="manicure")
 	private String manicure;
+	@Column(name="pedicure")
 	private String pedicure;
+	@Column(name="francesinha")
 	private String francesinha;
+	@Column(name="esmaltar")
 	private String esmaltar;
+	@Column(name="pernaint")
 	private String pernaint;
+	@Column(name="perna12")
 	private String perna12;
+	@Column(name="virilha")
 	private String virilha;
+	@Column(name="intima")
 	private String intima;
+	@Column(name="axila")
 	private String axila;
+	@Column(name="sobrancelha")
 	private String sobrancelha;
+	@Column(name="buco")
 	private String buco;
+	@Column(name="pedicurePreco")
 	private String pedicurePreco;
+	@Column(name="francesinhaPreco")
 	private String francesinhaPreco;
+	@Column(name="esmaltarpreco")
 	private String esmaltarPreco;
+	@Column(name="pernaintpreco")
 	private String pernaintPreco;
+	@Column(name="perna12preco")
 	private String perna12Preco;
+	@Column(name="virilhapreco")
 	private String virilhaPreco;
+	@Column(name="intimapreco")
 	private String intimaPreco;
+	@Column(name="axilapreco")
 	private String axilaPreco;
+	@Column(name="sobrancelhapreco")
 	private String sobrancelhaPreco;
+	@Column(name="bucopreco")
 	private String bucoPreco;
+	@Column(name="preco")
 	private String preco;
+	@Column(name="formapagamento")
 	private String formaPagamento;
+	@Column(name="datapesquisainicio")
 	private String dataPesquisaInicio;
+	@Column(name="datapesquisafim")
 	private String dataPesquisaFim;
+	@Column(name="nomepesquisacliente")
 	private String nomePesquisaCliente;
+	@Column(name="nomepesquisafuncionario")
 	private String nomePesquisaFuncionario;
 
 	public String getDataPesquisaInicio() {
