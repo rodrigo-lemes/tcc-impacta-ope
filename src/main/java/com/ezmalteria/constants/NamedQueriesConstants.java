7122e3040cff958e86d4f65c77a2cca8463cb544
Rodrigo Soukef <rodrigo.lemes@gmail.com>
2017-11-19 14:57:42 -0200

    custom query for product

---
+++ src/main/java/com/ezmalteria/constants/NamedQueriesConstants.java
@@ -0,0 +1,10 @@
+package com.ezmalteria.constants;
+
+public final class NamedQueriesConstants {
+
+	private NamedQueriesConstants() {}
+	
+	public static final String GET_LOW_STOCK_PRODUCTS = "SELECT p FROM produto p WHERE p.quantidade > :quantity";
+	public static final String GET_LOW_STOCK_PRODUCTS_QUANTITY_FIELD_PARAMETER_NAMING = "quantity";
+	
+}
--- src/main/java/com/ezmalteria/dao/ProdutoDao.java
+++ src/main/java/com/ezmalteria/dao/ProdutoDao.java
@@ -7,12 +7,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.ezmalteria.constants.NamedQueriesConstants;
 import com.ezmalteria.domain.ProdutoTO;
 
 @Repository
 public interface ProdutoDao extends CrudRepository<ProdutoTO, Long> {
 
-	@Query("SELECT p FROM produto p WHERE p.quantidade > :quantity")
-	public List<ProdutoTO> findAllLowStockProducts(@Param("quantity") String quantity);
-	
+	@Query(NamedQueriesConstants.GET_LOW_STOCK_PRODUCTS)
+	public List<ProdutoTO> findAllLowStockProducts(
+			@Param(NamedQueriesConstants.GET_LOW_STOCK_PRODUCTS_QUANTITY_FIELD_PARAMETER_NAMING) String quantity);
 }
