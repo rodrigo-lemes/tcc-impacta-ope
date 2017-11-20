16688947219a301a5ed578e6b5984392534481c7
rodrigo@proponto-dev <ti.03@proponto.com.br>
2017-11-20 11:34:06 -0200

    fix jpa usage

---
+++ src/main/java/com/ezmalteria/constants/NamedQueriesConstants.java
@@ -0,0 +1,10 @@
+package com.ezmalteria.constants;
+
+public final class NamedQueriesConstants {
+
+	private NamedQueriesConstants() {}
+	
+	public static final String GET_LOW_STOCK_PRODUCTS_QUERY = "SELECT p FROM produto p WHERE p.quantidade < :quantity";
+	public static final String GET_LOW_STOCK_PRODUCTS_QUANTITY_WHERE_FILTER_FIELD = "quantity";
+	
+}
--- src/main/java/com/ezmalteria/dao/ProdutoDao.java
+++ src/main/java/com/ezmalteria/dao/ProdutoDao.java
@@ -7,13 +7,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.ezmalteria.constants.QueriesConstants;
+import com.ezmalteria.constants.NamedQueriesConstants;
 import com.ezmalteria.domain.ProdutoTO;
 
 @Repository
 public interface ProdutoDao extends CrudRepository<ProdutoTO, Long> {
 
-	@Query(value = QueriesConstants.GET_LOW_STOCK_PRODUCTS_QUERY,nativeQuery = true)
+	@Query(value = NamedQueriesConstants.GET_LOW_STOCK_PRODUCTS_QUERY)
 	public List<ProdutoTO> findAllLowStockProducts(
-			/*@Param(QueriesConstants.GET_LOW_STOCK_PRODUCTS_QUANTITY_WHERE_FILTER_FIELD) String quantity*/);
+			@Param(NamedQueriesConstants.GET_LOW_STOCK_PRODUCTS_QUANTITY_WHERE_FILTER_FIELD) Integer quantity);
 }
